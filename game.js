1062ac17952c780710fe26ce613f695399aae1f9
SUMIT SINGH <110274267+Basic455@users.noreply.github.com>
2024-09-21 10:14:03 +0000

    second-draft

--- game.js
+++ game.js
@@ -1,65 +1,51 @@
-//get computer choice
-function getComputerChoice() {
-    let randomNumber = parseInt(Math.random()*3 + 1)
-    if (randomNumber === 1){
-        return 'rock'
-    } else if (randomNumber === 2){
-        return 'paper'
-    } else {
-        return 'scissor'
+function hChoice() {
+    return prompt("Enter your choice");
+  }
+
+  function cChoice() {
+    let randomNumber = parseInt(Math.random() * 3);
+    const choices = ["rock", "paper", "scissor"];
+    return choices[randomNumber];
+  }
+
+  //
+  let hScore = 0;
+  let CScore = 0;
+
+  function gameLogic(human, computer) {
+    if (human === computer) {
+    } else if (human === "rock") {
+      if (computer === "paper") {
+        CScore++;
+      } else {
+        hScore++;
+      }
+    } else if (human === "paper") {
+      if (computer === "scissor") {
+        CScore++;
+      } else {
+        hScore++;
+      }
+    } else if (human === "scissor") {
+      if (computer === "rock") {
+        CScore++;
+      } else {
+        hScore++;
+      }
     }
-}
+  }
 
-//get user choice
-function getHumanChoice() {
-    let choice = prompt('Enter your choice: rock, paper or scissor')
-    choice = choice.trim().toLowerCase()
-    return choice
-}
+  for (let index = 0; index < 5; index++) {
+    let human = hChoice();
+    let computer = cChoice();
+    // let hChoice = prompt("Enter Your Choice");
+    gameLogic(human, computer);
+  }
 
-
-//compare
-let humanScore = 0;
-let computerScore = 0;
-
-//game logic
-function playRound(player, computer){
-    if (player == 'rock'){
-        if (computer == 'paper'){
-            return computerScore++;
-        } else {
-            console.log('You Win')
-            return humanScore++;
-        }
-    } else if (player == 'paper'){
-        if(computer == 'scissor'){
-
-            return computerScore++;
-        } else {
-            return humanScore++;
-        }
-    } else if (player == "scissor"){
-        if (computer == 'rock'){
-            return computerScore++;
-        } else {
-
-            return humanScore++;
-        }
-    }
-
-}
-
-
-function playGame() {
-    for(let i = 0; i <= 5; i++){
-        playRound(getHumanChoice(), getComputerChoice());
-    }
-    if (humanScore > computerScore){
-        console.log(`you won. your score was ${humanScore}`)
-    }
-    else {
-        console.log(`you lost. your score was ${humanScore}`)
-    }
-}
-
-playGame()
+  if (hScore > CScore) {
+    console.log(`you won ${hScore}/5`);
+  } else if (hScore === CScore) {
+    console.log(`draw ${hScore}/5 ${CScore}/5`);
+  } else {
+    console.log("YOU LOSE TT");
+  }
