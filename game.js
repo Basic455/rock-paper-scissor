abf03533c999675809405d705dd3c785502d5e5c
Basic455 <noobcreator47@gmail.com>
2024-09-23 11:11:23 +0530

    added ui to the game

--- game.js
+++ game.js
@@ -1,51 +1,80 @@
-function hChoice() {
-    return prompt("Enter your choice");
-  }
+const rockButton = document.querySelector(".rock")
+const paperButton = document.querySelector(".paper")
+const scissorButton = document.querySelector(".scissor")
+const result = document.querySelector(".result")
 
-  function cChoice() {
-    let randomNumber = parseInt(Math.random() * 3);
-    const choices = ["rock", "paper", "scissor"];
-    return choices[randomNumber];
-  }
+let hScore = 0;
+let CScore = 0;
 
-  //
-  let hScore = 0;
-  let CScore = 0;
+function cChoice() {
+  let randomNumber = parseInt(Math.random() * 3);
+  const choices = ["rock", "paper", "scissor"];
+  return choices[randomNumber];
+}
 
-  function gameLogic(human, computer) {
-    if (human === computer) {
-    } else if (human === "rock") {
-      if (computer === "paper") {
-        CScore++;
-      } else {
-        hScore++;
-      }
-    } else if (human === "paper") {
-      if (computer === "scissor") {
-        CScore++;
-      } else {
-        hScore++;
-      }
-    } else if (human === "scissor") {
-      if (computer === "rock") {
-        CScore++;
-      } else {
-        hScore++;
-      }
+function gameLogic(human, computer) {
+  if (human === computer) {
+  } else if (human === "rock") {
+    if (computer === "paper") {
+      CScore++;
+    } else {
+      hScore++;
+    }
+  } else if (human === "paper") {
+    if (computer === "scissor") {
+      CScore++;
+    } else {
+      hScore++;
+    }
+  } else if (human === "scissor") {
+    if (computer === "rock") {
+      CScore++;
+    } else {
+      hScore++;
     }
   }
+}
 
-  for (let index = 0; index < 5; index++) {
-    let human = hChoice();
-    let computer = cChoice();
-    // let hChoice = prompt("Enter Your Choice");
-    gameLogic(human, computer);
-  }
 
-  if (hScore > CScore) {
-    console.log(`you won ${hScore}/5`);
+rockButton.addEventListener('click', function(e){
+   const hChoice = e.target.value
+   console.log(hChoice)
+   gameLogic(hChoice, cChoice())
+   if (hScore > CScore) {
+    result.textContent = `you won computer picked ${cChoice()} and your score is ${hScore}`;
   } else if (hScore === CScore) {
-    console.log(`draw ${hScore}/5 ${CScore}/5`);
+    result.textContent = `draw computer picked ${cChoice()} and your score is ${hScore}`;
   } else {
-    console.log("YOU LOSE TT");
+    result.textContent = `YOU LOSE TT computer picked ${cChoice()} and your score is ${hScore}`;
   }
+
+});
+
+paperButton.addEventListener('click', function(e){
+  const hChoice = e.target.value
+  console.log(hChoice)
+  gameLogic(hChoice, cChoice())
+  if (hScore > CScore) {
+   result.textContent = `you won computer picked ${cChoice()} and your score is ${hScore}`;
+ } else if (hScore === CScore) {
+   result.textContent = `draw computer picked ${cChoice()} and your score is ${hScore}`;
+ } else {
+   result.textContent = `YOU LOSE TT computer picked ${cChoice()} and your score is ${hScore}`;
+ }
+
+});
+
+scissorButton.addEventListener('click', function(e){
+  const hChoice = e.target.value
+  console.log(hChoice)
+  gameLogic(hChoice, cChoice())
+  if (hScore > CScore) {
+   result.textContent = `you won computer picked ${cChoice()} and your score is ${hScore}`;
+ } else if (hScore === CScore) {
+   result.textContent = `draw computer picked ${cChoice()} and your score is ${hScore}` ;
+ } else {
+   result.textContent = `YOU LOSE TT computer picked ${cChoice()} and your score is ${hScore}`;
+ }
+
+});
+
